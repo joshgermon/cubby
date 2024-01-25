f99083f8d5a7498a042f153e189a4ed82c25961f
Joshua Germon <joshgermon@gmail.com>
2024-01-25 21:50:23 +1100

    Move around struct definition

--- include/backup.h
+++ include/backup.h
@@ -6,14 +6,6 @@
 
 #include "cubby.h"
 
-typedef struct {
-  char *id_name;
-  char *uuid;
-  char *size;
-  char *block_size_part_table_type;
-  char *syspath;
-} device_attrs;
-
 int backup_dir(cubby_opts_t *opts, const char *source_dir);
 const char* mount_device_to_fs(const char *dev_name);
 void unmount_device(const char *mount_path);
--- include/monitor.h
+++ include/monitor.h
@@ -4,8 +4,16 @@
 #include "cubby.h"
 #include <systemd/sd-device.h>
 
+typedef struct {
+  char *id_name;
+  char *uuid;
+  char *size;
+  char *block_size_part_table_type;
+  char *syspath;
+} device_attrs;
+
 int setup_udev_monitoring(cubby_opts_t *opts);
 int get_list_of_available_devices();
-const char* get_largest_partition(const char *devpath);
+device_attrs get_sdcard_attributes(sd_device *dev);
 
 #endif /* MONITOR_H */
