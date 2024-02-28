6969b30131b6457b0a15ecfe19f023005e4098cd
Joshua <joshgermon@gmail.com>
2024-02-28 10:42:15 +0000

    Removed unneeded valid cmd check

--- src/cli.c
+++ src/cli.c
@@ -3,8 +3,6 @@
 #include <getopt.h>
 #include <string.h>
 
-static char *valid_commands[3] = {"list", "start", "help"};
-
 static struct option default_opts[] = {
     {"verbose", no_argument, 0, 'v'},
     {0, 0, 0, 0}
@@ -16,7 +14,7 @@ static struct option start_opts[] = {
     {0, 0, 0, 0}
 };
 
-void parse_default_args(int argc, char **argv, cubby_opts_t *opts) {
+static void parse_default_args(int argc, char **argv, cubby_opts_t *opts) {
   int opt;
   int opt_index = 0;
 
@@ -31,7 +29,7 @@ void parse_default_args(int argc, char **argv, cubby_opts_t *opts) {
   }
 }
 
-void parse_start_opts(int argc, char **argv, cubby_opts_t *opts) {
+static void parse_start_opts(int argc, char **argv, cubby_opts_t *opts) {
   int opt;
   int opt_index = 0;
 
@@ -51,20 +49,9 @@ void parse_start_opts(int argc, char **argv, cubby_opts_t *opts) {
   if (opts->backup_path == NULL) die("Backup path is required.\n");
 }
 
-static int ensure_valid_command(char *command) {
-  for(int i = 0; i < 3; i++) {
-    if (strcmp(command, valid_commands[i]) == 0) return 0;
-  }
-  return -1;
-}
-
-
 void parse_args(int argc, char **argv, cubby_opts_t *opts) {
   opts->command = argv[1];
   if (opts->command == NULL) return;
-
-  if (!ensure_valid_command(opts->command))
-    die("Command is not a valid command, run 'cubby help' to see valid usage.");
 
   if (strcmp(opts->command, "list") == 0) {
     parse_default_args(argc, argv, opts);
@@ -72,5 +59,7 @@ void parse_args(int argc, char **argv, cubby_opts_t *opts) {
     parse_default_args(argc, argv, opts);
   } else if (strcmp(opts->command, "start") == 0) {
     parse_start_opts(argc, argv, opts);
+  } else {
+    die("Command is not a valid command, run 'cubby help' to see valid usage.");
   }
 }
